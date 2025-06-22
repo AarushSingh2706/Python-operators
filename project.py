0e1f1eb76dacd5f0d0059aea1482793d776a0ad5
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-22 14:55:24 +0530

    project

---
+++ project.py
@@ -0,0 +1,22 @@
+a = input("Enter any value from a to i")
+if a == "A" or a == "a" :
+    print("You have entered an alphabet")
+elif a == "B" or a == "b" :
+    print("You have entered an alphabet")
+elif a == "C" or a == "c" :
+    print("You have entered an alphabet")
+elif a == "D" or a == "d" :
+    print("You have entered an alphabet")
+elif a == "E" or a == "e" :
+    print("You have entered an alphabet")
+elif a == "F" or a == "f" :
+    print("You have entered an alphabet")
+elif a == "G" or a == "g" :
+    print("You have entered an alphabet")
+elif a == "H" or a == "h" :
+    print("You have entered an alphabet")
+elif a == "I" or a == "i" :
+    print("You have entered an alphabet")
+else : 
+    print("Invalid input")
+    
