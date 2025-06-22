a31232bf090a3759cdb320a7e4b08111bccd274e
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-22 14:37:38 +0530

    not statement

---
+++ not.py
@@ -0,0 +1,20 @@
+a = 10
+b = 15 
+c = 15
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+if a != b : 
+    print("Hello")
+
+a = int(input("Enter any number"))
+if a != 20 : 
+    print("Hello")
+
+a = 15 
+if a%2 == 0 :
+    print("it is an even number")
+else : 
+    print("It is an odd number")
