842c6f6ceecaca09e64364215f2a79f5cd30e2f0
AarushSingh2706 <nisha2008mb0035@gmail.com>
2025-06-22 14:27:54 +0530

    and,or statements

---
+++ and,or.py
@@ -0,0 +1,12 @@
+a = 10
+b = -10
+c = 0
+if a > 0 and b > 0 : 
+    print("Both number is greater than ")
+else : 
+    print("Only one value is true")
+
+if a > 0 or c > 0 : 
+    print("One value is true")
+else : 
+    print("One value is false")
